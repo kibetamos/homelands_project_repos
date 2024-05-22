648478b47daaf732953d00a39fa6532e4796a80c
kibetamos <amos.rotich@strathmore.edu>
2024-05-22 17:55:56 +0300

    added files

---
+++ Scraper Status.sql
@@ -0,0 +1,36 @@
+====SERVER on Friday 13th Jan=====
+
+FROM 100,000 TO 400,000
+===13 JAN 2023=== 
+========START =====
+Scraper updates
+
+cases_100000_150000.json 
+
+
+cases_151000_200000.json
+
+
+cases_201000_400000.json
+
+========END =====
+
+
+====SERVER on Monday 23rd Jan 2023=====
+Scraper updates
+REDO ALL THESE
+cases_100000_110000.json 
+
+
+cases_151000_200000.json
+
+
+cases_201000_400000.json - started
+
+progress
+100000-110000 - ongoing
+110-12000- started
+
+====SERVER on FRIDAY 27TH Jan 2023=====
+--Scrape updates--
+121-130,000 - started
